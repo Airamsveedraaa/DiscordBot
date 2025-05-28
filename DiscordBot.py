a49e4c2bbd12c85c3037e8e7287983e7b9b845c9
Airamsveedraaa <l.saavedrap5@gmail.com>
2025-05-28 09:36:35 +0200

    Changed enviroment variable, now it uses one to take the token

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,3 +1,4 @@
+import os
 import discord
 
 class MyClient(discord.Client):
@@ -15,4 +16,4 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
-client.run('MTM3NzE3Nzg5NjA1Mjk4MTgxMA.GQoihH.4JjG7VR1Ai_yUqibl8q0aKSi-X3H6vjtsue9I4')
+client.run(os.getenv("DISCORD_TOKEN"))
